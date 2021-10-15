925221d51f05017268f300b70e56ce24d58ea819
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2021-10-15 11:57:56 -0500

    lab work

--- Unit4_loops/examples/PigLatinLab/PigLatin.java
+++ Unit4_loops/examples/PigLatinLab/PigLatin.java
@@ -1,17 +1,30 @@
 package Unit4_loops.examples.PigLatinLab;
 
+import java.util.Locale;
+
 public class PigLatin {
-<<<<<<< HEAD
 
-=======
-    //public static String toPigLatin(String phrase){
-
-    //}
+    public static String toPigLatin(String phrase){
+        int index = 0;
+        String tempPhrase = phrase;
+        String translatedPhrase = "";
+        String aWord = "";
+        while(index != -1){
+            index = tempPhrase.indexOf(" ");
+            aWord = tempPhrase.substring(0,index);
+            tempPhrase = tempPhrase.substring(index);
+            translatedPhrase += aWord + " ";
+        }
+        return translatedPhrase;
+    }
 
     public static String translateWordToPigLatin(String str){
         String translatedWord = "";
-        if (vowelCheck(str) == true){
+        if (vowelCheck(str.substring(0,1)) == true) {
             translatedWord = str + "yay";
+        } else if(vowelCheck(str.substring(0,1)) == false && vowelCheck(str.substring(1,2)) == false){
+            String twoLetters = str.substring(0,2);
+            translatedWord = str.substring(2) + twoLetters + "ay";
         } else {
             String firstLetter = str.substring(0,1);
             translatedWord = str.substring(1) + firstLetter + "ay";
@@ -21,19 +34,20 @@ public class PigLatin {
 
 
     public static boolean vowelCheck(String word){
-        if(word.substring(0,1).equals("a")){
+        word = word.toLowerCase(Locale.ROOT);
+        if(word.equals("a")){
             return true;
-        } else if(word.substring(0,1).equals("e")){
+        } else if(word.equals("e")){
             return true;
-        } else if(word.substring(0,1).equals("i")){
+        } else if(word.equals("i")){
             return true;
-        } else if(word.substring(0,1).equals("o")){
+        } else if(word.equals("o")){
             return true;
-        } else if(word.substring(0,1).equals("u")){
+        } else if(word.equals("u")){
             return true;
         } else{
             return false;
         }
     }
->>>>>>> f960d6d434bde28f8a27a54108179d39bb3ff20e
+
 }
