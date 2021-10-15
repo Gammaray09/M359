f960d6d434bde28f8a27a54108179d39bb3ff20e
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2021-10-14 19:54:07 -0500

    lab work

--- Unit4_loops/examples/PigLatinLab/PigLatin.java
+++ Unit4_loops/examples/PigLatinLab/PigLatin.java
@@ -1,5 +1,35 @@
 package Unit4_loops.examples.PigLatinLab;
 
 public class PigLatin {
-    
+    //public static String toPigLatin(String phrase){
+
+    //}
+
+    public static String translateWordToPigLatin(String str){
+        String translatedWord = "";
+        if (vowelCheck(str) == true){
+            translatedWord = str + "yay";
+        } else {
+            String firstLetter = str.substring(0,1);
+            translatedWord = str.substring(1) + firstLetter + "ay";
+        }
+        return translatedWord;
+    }
+
+
+    public static boolean vowelCheck(String word){
+        if(word.substring(0,1).equals("a")){
+            return true;
+        } else if(word.substring(0,1).equals("e")){
+            return true;
+        } else if(word.substring(0,1).equals("i")){
+            return true;
+        } else if(word.substring(0,1).equals("o")){
+            return true;
+        } else if(word.substring(0,1).equals("u")){
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
