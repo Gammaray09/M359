c155478b105e19ca8c5b4608dc315a48201aa2a6
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2021-10-27 11:58:31 -0500

    battle

--- Unit5_WritingClasses/examples/HeroVillainExamples/MarvelMovieTester.java
+++ Unit5_WritingClasses/examples/HeroVillainExamples/MarvelMovieTester.java
@@ -2,16 +2,55 @@ package Unit5_WritingClasses.examples.HeroVillainExamples;
 
 public class MarvelMovieTester {
     public static void main(String[] args) {
-        Power lighting = new Power("lighting", 8);
-        Power strength = new Power("Super Strength", 10);
+        Power lighting = new Power("lighting", 8.0);
+        Power strength = new Power("Super Strength", 15.0);
 
         SuperHero Thor = new SuperHero("Thor", lighting);
-        SuperHero Captain_America = new SuperHero("Captain America", strength);
         Villain Thanos = new Villain("Thanos", strength);
 
-        Thanos.getSuperPower().setStrength(15);
 
         System.out.println(Thanos);
-        System.out.println(Captain_America);
+        System.out.println(Thor);
+
+
+    }
+
+    /**
+     * This simulates a battle between hero and villain
+     *      * which ever has the greater strength
+     *      *      Winner will gain 10% of the strength of the loser
+     *      *      Loser will lose 8-30% of there own Strength
+     *      *      If it is a tie. they both will lose 8-30% of there own Strength
+     *      *      print result afterword
+     * @param hero
+     * @param villain
+     */
+    public static void battle (SuperHero hero, Villain villain){
+        double heroStrength = hero.getSuperPower().getStrength();
+        double villainStrength = hero.getSuperPower().getStrength();
+        double random = (int)(Math.random() * 31 + 5);
+        String winner = "";
+        // hero wins
+        if(heroStrength > villainStrength){
+            // take 10% of villain power
+            heroStrength =(villainStrength * 0.10) + heroStrength;
+            villainStrength =(villainStrength - random * 0.01) + villainStrength;
+            winner = "The winner is" + hero.getName();
+        // villain wins
+        } else if(heroStrength > villainStrength){
+            villainStrength =(heroStrength * 0.10) + villainStrength;
+            heroStrength =(heroStrength - random * 0.01) + heroStrength;
+            winner = "The winner is";
+        // both tie
+        } else {
+            villainStrength =(villainStrength * random) + villainStrength;
+            heroStrength = (heroStrength - random * 0.01) + heroStrength;
+            winner = "Its a Tie";
+        }
+        // Now that we are done calculating the power set it
+        hero.getSuperPower().setStrength(heroStrength);
+        villain.getSuperPower().setStrength(villainStrength);
+        System.out.println(winner);
+
     }
 }
--- Unit5_WritingClasses/examples/HeroVillainExamples/Power.java
+++ Unit5_WritingClasses/examples/HeroVillainExamples/Power.java
@@ -2,17 +2,17 @@ package Unit5_WritingClasses.examples.HeroVillainExamples;
 
 public class Power {
     private String power;
-    private int strength;
+    private double strength;
 
-    public Power(String power, int strength) {
+    public Power(String power, double strength) {
         this.power = power;
         this.strength = strength;
     }
 
     public String getPower() { return power; }
     public void setPower(String power) { this.power = power; }
-    public int getStrength() { return strength; }
-    public void setStrength(int strength) { this.strength = strength; }
+    public double getStrength() { return strength; }
+    public void setStrength(double strength) { this.strength = strength; }
 
     public String toString() { return "Power:" + power + "\n" + "Strength:" + strength ; }
 }
