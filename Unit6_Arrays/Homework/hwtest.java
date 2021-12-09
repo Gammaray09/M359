4083e613dca19878ead11e4ec29e47c4f552028c
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2021-12-08 19:22:11 -0600

    test

--- Unit6_Arrays/Homework/hwtest.java
+++ Unit6_Arrays/Homework/hwtest.java
@@ -1,5 +1,12 @@
 package Unit6_Arrays.Homework;
 
 public class hwtest {
+    public static void main(String[] args) {
+        int[] arr = {56,24,786,345,6432};
+        System.out.println(arr.length);
 
+
+        int[] newArr = new int[5];
+        System.out.println(newArr.length);
+    }
 }
