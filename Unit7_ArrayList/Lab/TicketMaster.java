d50189f7824d57232e2195b35d66c0def440fa7f
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2022-01-13 11:58:53 -0600

    lab start

---
+++ Unit7_ArrayList/Lab/TicketMaster.java
@@ -0,0 +1,10 @@
+package Unit7_ArrayList.Lab;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TicketMaster {
+    private ArrayList<Show> showsList = new ArrayList<Show>();
+
+    Scanner fileIn = new Scanner();
+}
