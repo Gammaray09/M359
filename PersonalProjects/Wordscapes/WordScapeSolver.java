5077b279bc8cac44efd238732e579c014810faf1
Aashman Sharma <90787261+Gammaray09@users.noreply.github.com>
2022-04-05 11:58:00 -0500

    e

--- PersonalProjects/Wordscapes/WordScapeSolver.java
+++ PersonalProjects/Wordscapes/WordScapeSolver.java
@@ -1,24 +1,65 @@
 package PersonalProjects.Wordscapes;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Array;
+import java.util.Scanner;
+
 public class WordScapeSolver {
-    public static void main(String[] args) {
-        String letters = "force";
+
+    public static void main(String[] args) throws FileNotFoundException{
+        String[] arr= new Array();
+        File words = new File("words_alpha.txt");
+        Scanner scnr = new Scanner(words);
+
+        while(scnr.hasNextLine()){
+            String curword = scnr.nextLine();
+
+            if(curword.equalsIgnoreCase(word)){
+                exist = true;
+                break;
+            }
+        }
+
+        String letters = "epsru";
         solver("",letters);
     }
 
 
-    public static void solver(String root, String l){
+    public static void solver(String root, String l)throws FileNotFoundException{
         if(l.length() == 0){
-            System.out.println(root);
+            if(checkWord(root) == true && root.length() > 3){
+                System.out.println(root);
+            }
         }
         for (int i = 0; i < l.length(); i++) {
-            System.out.println(root);
+            if(checkWord(root) == true && root.length() > 3){
+                System.out.println(root);
+            }
             solver(root + l.charAt(i), l.substring(0, i) + l.substring(i + 1));
         }
 
     }
 
-    public static boolean checkWord(String word){
-        return false;
+    public static boolean checkWord(String word) throws FileNotFoundException {
+        File words = new File("words_alpha.txt");
+        Scanner scnr = new Scanner(words);
+
+
+        boolean exist = false;
+
+        while(scnr.hasNextLine()){
+            String curword = scnr.nextLine();
+
+            if(curword.equalsIgnoreCase(word)){
+                exist = true;
+                break;
+            }
+        }
+        return exist;
+    }
+
+
+    public static void binarySearch() throws FileNotFoundException{
     }
 }
